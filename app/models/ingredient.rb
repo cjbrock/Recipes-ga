e0aa784a8eab462c8cf3d4f05f10ad5fb7ded6a5
Corinna Brock <corinna.brock@gmail.com>
2012-07-29 16:34:14 -0400

    Commit 2: The Ingredient Resource
    Generate a scaffold for the Ingredient Resource. An ingredient should
    be able to have a name.

---
+++ app/models/ingredient.rb
@@ -0,0 +1,3 @@
+class Ingredient < ActiveRecord::Base
+  attr_accessible :name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RecipesGa::Application.routes.draw do
+  resources :ingredients
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
