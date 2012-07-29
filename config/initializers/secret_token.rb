2c1d7766ee23bcf322df0ff3d14bb0134ec40895
Corinna Brock <corinna.brock@gmail.com>
2012-07-29 16:15:54 -0400

    First Commit
    1. Generate a new Rails application into the directory recipes-ga.
    2. Add that initial application to the repository and commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecipesGa::Application.config.secret_token = 'deb72d3d24f78ae8b7d54795aa7dc84683fde8b067387b40dd2c5d6b4ed53ab571dc744ff493cabf6d944ea052d74ab2ad91e3ae7ba962e52e8e361f7cfc803a'
