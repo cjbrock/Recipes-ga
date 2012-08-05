af1cd92479ba94856a2f0d30ad4ac06767905ff3
Corinna Brock <corinna.brock@gmail.com>
2012-08-05 16:35:33 -0400

    login filter applied properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #before_filter :login_required
 
-  #def login_required
-   #   redirect_to login_path, :notice => "Please sign in" unless logged_in?
-    #end
-  #end
+  before_filter :login_required
 
-  #def logged_in
-   # false
-  #end
+  def login_required
+    redirect_to login_path, :notice => "Please sign in" unless logged_in?
+  end
+
+  def logged_in?
+    current_user ? true : false
+    #!! current_user
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  #skip_before_filter :login_required, :only => [:new, :create]
+  skip_before_filter :login_required, :only => [:new, :create]
   def new
 
   end
