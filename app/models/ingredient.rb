47849510ea257a7789a738b7634b6013a8f8ff30
Corinna Brock <corinna.brock@gmail.com>
2012-08-09 10:59:17 -0400

    fixing the recipe_ingredients mess

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,8 @@
 class Ingredient < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :recipes
-  #has_many :recipeingredients - needs to be a has manythrough
+  has_many :recipe_ingredients
+  has_many :recipes, :through => :recipe_ingredients
 
   validates_uniqueness_of :name, :case_sensitive => false
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,9 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :description, :name
 
-  has_many :ingredients
-  has_many :recipeingredients
+  
+  has_many :recipe_ingredients
+  has_many :ingredients, :through => :recipe_ingredients
+
+  accepts_nested_attributes_for :ingredients, allow_destroy: true
 end
