117c05dfa3e2f042deab35df448b0a66239da52b
Corinna Brock <corinna.brock@gmail.com>
2012-08-14 11:20:49 -0400

    working on nested form

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,8 +1,12 @@
 class RecipeIngredient < ActiveRecord::Base
-  attr_accessible :measurement_unit, :quantity
+  attr_accessible :measurement_unit, :quantity, :ingredient, :ingredient_id
   belongs_to :recipe
   belongs_to :ingredient
 
   accepts_nested_attributes_for :recipe, allow_destroy: true
   accepts_nested_attributes_for :ingredient, allow_destroy: true
+
+  def ingredient=(ingredient)
+    self.ingredient_id = Ingredient.find_or_create_by_name(ingredient.capitalize).id
+  end
 end
